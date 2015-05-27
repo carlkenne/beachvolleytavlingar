0b3a6492042bbca349c4cbbdd82a9d8a84fbdb72
carl kenne <carl.kenne@gmail.com>
2015-05-27 11:51:11 +0200

    Initial Commit

---
+++ BVTournamentCalendar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BVTournamentCalendar
+//
+//  Created by Carl Kenne on 27/05/15.
+//  Copyright (c) 2015 Carl Kenne. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
