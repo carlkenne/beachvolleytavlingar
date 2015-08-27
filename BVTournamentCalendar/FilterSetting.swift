53304ebf753522497712efe0530d4b68ead47b4c
Carl Kenne <carl.kenne@gmail.com>
2015-08-27 11:01:53 +0200

    version 1.4

---
+++ BVTournamentCalendar/FilterSetting.swift
@@ -0,0 +1,9 @@
+//
+//  FilterSetting.swift
+//  BVTournamentCalendar
+//
+//  Created by Carl Kenne on 17/08/15.
+//  Copyright (c) 2015 Carl Kenne. All rights reserved.
+//
+
+import Foundation
---
+++ BVTournamentCalendar/PlayerRankingVC.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerRankingVC.swift
+//  BVTournamentCalendar
+//
+//  Created by Carl Kenne on 14/08/15.
+//  Copyright (c) 2015 Carl Kenne. All rights reserved.
+//
+
+import Foundation
