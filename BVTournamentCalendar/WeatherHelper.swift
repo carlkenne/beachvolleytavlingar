db42482aaf7a971559c25b2a68c7566d3931fa13
Wilhelm Fors <askims_ik94w@hotmail.com>
2017-03-07 14:10:40 +0100

    WeatherHelper now writes weather for hardcoded time and place to console

--- BVTournamentCalendar/WeatherHelper.swift
+++ BVTournamentCalendar/WeatherHelper.swift
@@ -11,6 +11,10 @@ import Foundation
 class WeatherHelper {
     
     let gbg = Pos(lat: 57.70, long: 11.97)
+    var time = "2017-03-10T12:00:00Z"
+    var temp : Float = 0.0
+    var wind : Float = 0.0
+    var symbol = 0
     
     func getWeatherFromAPI(urlString: String) {
     
@@ -24,13 +28,65 @@ class WeatherHelper {
             
             do {
                 let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
-                let posts = json["timeSeries"] as? [[String: Any]]
-                //print(posts)
+                if let posts = json["timeSeries"] as? [[String: Any]] {
+                    for post in posts {
+                        let postTime = post["validTime"] as! String
+                        if postTime == self.time {
+                            if let parameters = post["parameters"] as? [[String : Any]] {
+                                let tempDict = parameters[1] as [String : Any]
+                                let tempValue = tempDict["values"] as! [Float]
+                                self.temp = tempValue[0]
+                                
+                                let windDict = parameters[4] as [String : Any]
+                                let windValue = windDict["values"] as! [Float]
+                                self.wind = windValue[0]
+                                
+                                print("Vind: \(windValue[0]) m/s")
+                                let symbDict = parameters[18] as [String : Any]
+                                let symbValue = symbDict["values"] as! [Float]
+                                self.symbol = Int(symbValue[0])
+                                
+                                print("10 mars 2017 kl. 08: \(self.getWeatherEmoji(symbolID: self.symbol))\(tempValue[0]) C")
+                                print("Vind: \(windValue[0]) m/s")
+                            } else {
+                                print("Parameters not found")
+                            }
+                        }
+                    }
+                } else {
+                    print("No timeSeries available")
+                }
+                
                 
             } catch let error as NSError {
                 print(error)
             }
         }).resume()
+    }
+    
+    func getWeatherEmoji(symbolID : Int) -> String {
+        switch symbolID {
+        case 1:
+            return "☀️"
+        case 2:
+            return "🌤"
+        case 3:
+            return "🌤"
+        case 4:
+            return "⛅️"
+        case 5:
+            return "🌥"
+        case 6, 7:
+            return "☁️"
+        case 8, 12:
+            return "🌧"
+        case 9, 13:
+            return "⛈"
+        case 10, 11, 14, 15:
+            return "❄️"
+        default:
+            return ""
+        }
     }
     
 }
